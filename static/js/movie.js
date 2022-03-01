df2d7b1adce172c485635b0d8007629ae50a0854
Yeop Jung <jjangyub@naver.com>
2022-03-02 00:41:04 +0900

    commit

--- static/js/movie.js
+++ static/js/movie.js
@@ -18,14 +18,14 @@ function search_mlist() {
                 key: '5b015a49a0f1ae63cd63b5a1ba139a86',
                 movieNm: search_movie,
             },
-            type: 'GET',
+            method : 'GET',
             timeout: 3000,
             // 결과로 전달되는 JSON은 문자열. 이 문자열은 처리하기가 힘듦.
             // javascript 객체로 변환
             dataType: 'json',
             // 만약 성공하면 아래의 함수가 호출
             success : function(result) {
-                console.log(reuslt)
+                alert('1_ajax 성공')
 
                 $('tbody').empty()
 
@@ -41,6 +41,117 @@ function search_mlist() {
                     let imgTd = $('<td></td>')
                     let searchTitle = result['movieListResult']['movieList'][i]['movieNm'] + " 포스터"
                     let movieCd = result['movieListResult']['movieList'][i]['movieCd']
+                    let img = $('<img />')
+                    imgTd.append(img)
+                    // $.ajax({
+                    //     async: true,
+                    //     url: 'https://dapi.kakao.com/v2/search/image',
+                    //     method: 'GET',
+                    //     headers: {
+                    //         Authorization: "KakaoAK " + 'cf0517506a09c39e970bb3cef7b670f9'
+                    //     },
+                    //     data: {
+                    //         query: searchTitle
+                    //     },
+                    //     timeout: 4000,
+                    //     dataType: 'json',
+                    //     success: function(result) {
+                    //         $('#myDiv').empty()
+                    //         let imgUrl = result['documents'][0]['thumbnail_url']
+                    //         img.attr('src',imgUrl)
+                    //     },
+                    //     error: function() {
+                    //         alert('먼가 이상해요!')
+                    //     }
+                    // })
+
+                    let openTd = $('<td></td>>').text(result['movieListResult']['movieList'][i]['openDt'])
+                    let genreTd = $('<td></td>>').text(result['movieListResult']['movieList'][i]['genreAlt'])
+                    let detailTd = $('<td></td>')
+                    let detailBtn = $('<button></button>').text('삭제').addClass('btn btn-warning')
+                    // detailBtn.on('click',function(){
+                    //     $.ajax({
+                    //         async : true,
+                    //         url: '/movie/detail/',
+                    //         type : GET,
+                    //         data : {
+                    //             // api에 넘겨야하는 데이터
+                    //
+                    //         },
+                    //         dataType : 'json',
+                    //         timeout : 3000,
+                    //         success : function(result){
+                    //             // <tr>
+                    //             //     <td>1,001</td>   //영화제목
+                    //             //     <td>data</td>    //영화제목 영어로
+                    //             //     <td>placeholder</td> //러닝타임
+                    //             //     <td>text</td>    // 감독님 이름
+                    //             //     <td>text</td>    // 개봉연도
+                    //             //     <td>text</td>    // 장르명
+                    //             //      <td>text</td>    // 배우이름-맡은 배역
+                    //             // </tr>
+                    //             let tr = $('<tr></tr>')
+                    //             alert('상세보기 버튼 눌림')
+                    //
+                    //         },
+                    //     error : function(){
+                    //             alert('먼가 이상해요!')
+                    //     }
+                    //     })
+                    // })
+
+                    detailTd.append(detailBtn)
+                    tr.append(titleTd)
+                    tr.append(imgTd)
+                    tr.append(openTd)
+                    tr.append(genreTd)
+                    tr.append(detailTd)
+                    $('tbody').append(tr)
+                }
+            },
+            // 만약 호출에 실패하면 아래의 함수가 호출
+            error: function () {
+                alert('먼가 이상해요!!')
+            }
+        });
+    }
+}
+
+function boxoffice_daily(){
+    let my_date = $('#searchDate').val()
+
+    if (my_date == ""){
+        alert('검색을 위해 날짜를 선택해 주세요!')
+    }
+
+    else{
+
+        let modified_date = my_date.replace(/-/g, '')
+
+        $.ajax({
+            async : true,
+            url: 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json',
+            data : {
+                key : '5b015a49a0f1ae63cd63b5a1ba139a86',
+                targetDt : modified_date
+            },
+            method : 'GET',
+            timeout : 3000,
+            dataType : 'json',
+            success : function(result) {
+                $('tbody').empty()
+
+                // <th scope="col">Rank</th>
+                // <th scope="col">포스터 이미지</th>
+                // <th scope="col">영화제목</th>
+                // <th scope="col">개봉일</th>
+                // <th scope="col">상세보기</th>
+
+                for(i=0;i<10;i++) {
+                    let tr = $('<tr></tr>')
+                    let rankTd = $('<td></td>').text(result['boxOfficeResult']['dailyBoxOfficeList'][i]['rank'])
+                    let imgTd = $('<td></td>>')
+                    let searchTitle = result['boxOfficeResult']['dailyBoxOfficeList'][i]['movieNm'] + " 포스터"
                     let img = $('<img />')
                     imgTd.append(img)
                     $.ajax({
@@ -64,73 +175,22 @@ function search_mlist() {
                             alert('먼가 이상해요!')
                         }
                     })
+                    let titleTd = $('<td></td>').text(result['boxOfficeResult']['dailyBoxOfficeList'][i]['movieNm'])
+                    let openTd = $('<td></td>').text(result['boxOfficeResult']['dailyBoxOfficeList'][i]['openDt'])
 
-                    let openTd = $('<td></td>>').text(result['movieListResult']['movieList'][i]['openDt'])
-                    let genreTd = $('<td></td>>').text(result['movieListResult']['movieList'][i]['genreAlt'])
-                    let detailTd = $('<td></td>')
-                    let detailBtn = $('<button></button>').text('삭제').addClass('btn btn-warning')
-                    detailBtn.on('click',function(){
-                        $.ajax({
-                            async : true,
-                            url: '/movie/detail/',
-                            type : GET,
-                            data : {
-                                // api에 넘겨야하는 데이터
-
-                            },
-                            dataType : 'json',
-                            timeout : 3000,
-                            success : function(result){
-                                // <tr>
-                                //     <td>1,001</td>   //영화제목
-                                //     <td>data</td>    //영화제목 영어로
-                                //     <td>placeholder</td> //러닝타임
-                                //     <td>text</td>    // 감독님 이름
-                                //     <td>text</td>    // 개봉연도
-                                //     <td>text</td>    // 장르명
-                                //      <td>text</td>    // 배우이름-맡은 배역
-                                // </tr>
-                                let tr = $('<tr></tr>')
-                                alert('상세보기 버튼 눌림')
-
-                            },
-                        error : function(){
-                                alert('먼가 이상해요!')
-                        }
-                        })
-                    })
-
-                    detailTd.append(detailBtn)
-                    tr.append(titleTd)
+                    tr.append(rankTd)
                     tr.append(imgTd)
+                    tr.append(titleTd)
                     tr.append(openTd)
-                    tr.append(genreTd)
-                    tr.append(detailTd)
                     $('tbody').append(tr)
                 }
+
             },
-            // 만약 호출에 실패하면 아래의 함수가 호출
-            error: function () {
-                alert('먼가 이상해요!!')
+            error : function (){
+                alert('먼가 이상해요!')
             }
+
         });
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
